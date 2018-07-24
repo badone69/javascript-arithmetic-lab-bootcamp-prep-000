624e3091eff5542ce917afc089326bab925b5cb5
student online <eliel.badon@gmail.com>
2018-07-24 02:52:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -28,18 +28,19 @@ function makeInt(a) {
   return parseInt(a,10)
 }
 
+function preserveDecimal(a) {
+  
+}
 
 
-describe('makeInt(n)', function() {
-  it('parses n as an integer and returns the parsed integer', function() {
-    expect(makeInt(a.toString())).toEqual(a)
-  })
 
-  it('assumes base 10', function() {
-    expect(makeInt('0x2328')).toEqual(0)
+describe('preserveDecimal(n)', function() {
+  it('preserves n\'s decimals (it parses n as a floating point number) and returns the parsed number', function() {
+    expect(preserveDecimal('2.222')).toBe(2.222)
   })
 
   it('returns NaN as appropriate', function() {
-    expect(isNaN(makeInt('sldkjflksjf'))).toEqual(true)
+    expect(isNaN(preserveDecimal('sldkjflksjf'))).toEqual(true)
   })
+})
 })
